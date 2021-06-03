1a4d520155e4930d37751312e08b979d7424c22e
Guillaume Fiorello <guillaume.fiorello@student.isae-supaero.fr>
2021-06-03 11:10:05 +0200

    Feat : Implement mutate

--- archive.py
+++ archive.py
@@ -1,9 +1,11 @@
+import random
+from numpy import array, mod, ones
 
 
 
 class Species:
     def init(self,genotype,pos,fitness):
-        self.genoptype = genotype
+        self.genotype = genotype
         self.pos = pos
         self.fitness = fitness
 
@@ -15,5 +17,36 @@ def addToArchive(archive, specie):
         archive[pos] = specie
 
 
-def mutate(archive):
-    pass
+def mutate(archive, cross = False, move_away = True):
+
+    if cross:
+        #Crossover
+        new_genotype = crossover(archive)
+    else :
+        specie = random.choice(list(archive.values))
+        new_genotype = specie.genotype
+
+    nb_genes = len(new_genotype)
+    array_of_ones = ones(nb_genes)
+
+    #Adding a random noise
+    scale_factor = 1
+    noise = random.randn(nb_genes)
+    new_genotype = mod(new_genotype \
+                     + array_of_ones \
+                     + noise * scale_factor, 2) \
+                   - array_of_ones
+
+    if move_away:
+        #Going away from another specie
+        scale_factor = 1
+        noise = random.randn(nb_genes))
+        other_specie = random.choice(list(archive.values))
+        other_genotype = other_specie.genotype
+        new_genotype = mod(new_genotype \
+                        + array_of_ones \
+                        + (new_genotype - other_genotype) * noise * scale_factor, 2) \
+                    - array_of_ones
+
+    return(new_genotype)
+
